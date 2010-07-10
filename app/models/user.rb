09814b8e0ca069376568fa6a6b86005a58008a28
Stéfanie Duprey <stefanie.duprey@gmail.com>
2010-07-10 22:28:41 +0200

    Adding some validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :login, :email, :first_name, :last_name, :presence => true
+  validates :login, :email, :uniqueness => true
+  validates_format_of :email, :with => %r{^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$}i
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "user attributes must not be empty" do
+    user = User.new
+    assert user.invalid?
+    assert user.errors[:login].any?
+    assert user.errors[:email].any?
+    assert user.errors[:first_name].any?
+    assert user.errors[:last_name].any?
+  end
+
+  test "user email must be a valid email" do
+    user = User.new(:login => 'mylogin',
+                    :first_name => 'myfirstname',
+                    :last_name => 'mylastname')
+
+    user.email = 'blabla.com'
+    assert user.invalid?
+    assert user.errors[:email].any?
+
+    user.email = 'blabla@blabla'
+    assert user.invalid?
+    assert user.errors[:email].any?
+
+    user.email = 'blabl/a@blabla'
+    assert user.invalid?
+    assert user.errors[:email].any?
+
+    user.email = 'blab_la@blabla'
+    assert user.invalid?
+    assert user.errors[:email].any?
+
+    user.email = 'blab la@blabla'
+    assert user.invalid?
+    assert user.errors[:email].any?
+
+    user.email = 'blabla@bla.com'
+    assert user.valid?
   end
 end
