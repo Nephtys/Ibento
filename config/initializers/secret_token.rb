6531f728038c5faf97d19d47cebbe1023177da2a
Stéfanie Duprey <stefanie.duprey@gmail.com>
2010-07-10 21:42:17 +0200

    Adding Base app and User scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '4516c1084d592ef462d4ccf5d46ac9e897fbe848be1148eb4afcce41ef5c3665323e1fcb0b8dce035c15faa1b9205a2928e5cf0ee617ad3224ae407cb3931e16'
